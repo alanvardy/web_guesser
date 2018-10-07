25d886dd5837902c45b37d6db5474ec822630b04
Alan Vardy <alanvardy@gmail.com>
2018-10-07 16:34:03 -0600

    refactor

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,9 +2,24 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 set :port, 9393
+set :secret_number, rand(100)
 
-number = rand(100)
+def check_guess(guess)
+  if guess == 0
+    ''
+  elsif guess < settings.secret_number
+    'Too low!'
+  elsif guess > settings.secret_number
+    'Too high!'
+  elsif guess == settings.secret_number
+    "You got it! The number is #{settings.secret_number}"
+  else
+    'Nothing'
+  end
+end
 
 get '/' do
-  erb :index, locals: { number: number }
+  guess = params['guess'].to_i
+  message = check_guess(guess)
+  erb :index, locals: { message: message }
 end
