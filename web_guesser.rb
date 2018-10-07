4491fb2cfcabe954d98c1092c3354c7adcf4a3b1
Alan Vardy <alanvardy@gmail.com>
2018-10-07 17:13:38 -0600

    added background color

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,20 +6,32 @@ set :secret_number, rand(100)
 
 def check_guess(guess)
   if guess == 0
-    ''
-  elsif guess < settings.secret_number
-    'Too low!'
-  elsif guess > settings.secret_number
-    'Too high!'
+    message = ''
+    color = 'white'
+  elsif settings.secret_number - guess < -5
+    message = 'Way too high!'
+    color = 'red'
+  elsif settings.secret_number - guess < 0
+    message = 'Too high!'
+    color = '#a26769'
+  elsif settings.secret_number - guess > 5
+    message = 'Way too low!'
+    color = 'red'
+  elsif settings.secret_number - guess > 0
+    message = 'Too low!'
+    color = '#a26769'
   elsif guess == settings.secret_number
-    "You got it! The number is #{settings.secret_number}"
+    message = "You got it! The number is #{settings.secret_number}"
+    color = 'green'
   else
-    'Nothing'
+    message = 'Nothing'
+    color = 'white'
   end
+  [message, color]
 end
 
 get '/' do
   guess = params['guess'].to_i
-  message = check_guess(guess)
-  erb :index, locals: { message: message }
+  message, color = check_guess(guess)
+  erb :index, locals: { message: message, color: color }
 end
